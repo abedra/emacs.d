f85e815ec627572ce5af2b172dec2147feb81341
Aaron Bedra <aaron@aaronbedra.com>
2012-09-28 09:45:06 -0500

    Backup starting init file incase regeneration needs to happen

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(copy-file
+ (concat user-emacs-directory "init.el")
+ (concat user-emacs-directory "init.el.base"))
+
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "init.org" user-emacs-directory))
