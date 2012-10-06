beaa2e60fb4bd270f769f28c9ea7f9982ad7585a
Aaron Bedra <aaron@aaronbedra.com>
2012-10-06 18:40:54 -0500

    Only backup init when it doesn't exist

--- init.el
+++ init.el
@@ -1,7 +1,13 @@
-(copy-file
- (concat user-emacs-directory "init.el")
- (concat user-emacs-directory "init.el.base"))
+(when (not
+     (file-exists-p
+      (concat user-emacs-directory "init.el.base")))
+    (copy-file
+     (concat user-emacs-directory "init.el")
+     (concat user-emacs-directory "init.el.base")))
 
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "init.org" user-emacs-directory))
+
+
+
