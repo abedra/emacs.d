80bccf050ac90a334878a3b6ca4126c0fc5771d4
Aaron Bedra <aaron@aaronbedra.com>
2012-10-07 17:15:35 -0500

    Clean up initial init.el

--- init.el
+++ init.el
@@ -1,13 +1,10 @@
 (when (not
-     (file-exists-p
-      (concat user-emacs-directory "init.el.base")))
-    (copy-file
-     (concat user-emacs-directory "init.el")
-     (concat user-emacs-directory "init.el.base")))
+       (file-exists-p
+        (concat user-emacs-directory "init.el.base")))
+  (copy-file
+   (concat user-emacs-directory "init.el")
+   (concat user-emacs-directory "init.el.base")))
 
 (require 'org-install)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "init.org" user-emacs-directory))
-
-
-
