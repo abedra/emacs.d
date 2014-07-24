52400047f062db324b0c79fcb86b348f112f7089
Aaron Bedra <aaron@aaronbedra.com>
2014-07-24 15:38:21 -0500

    Add writegood and flyspell modes to markdown-mode by default

--- abedra.el
+++ abedra.el
@@ -304,7 +304,11 @@
 
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
-(add-hook 'markdown-mode-hook (lambda () (visual-line-mode t)))
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (visual-line-mode t)
+            (writegood-mode t)
+            (flyspell-mode t)))
 (setq markdown-command "pandoc --smart -f markdown -t html")
 (setq markdown-css-path (expand-file-name "markdown.css" abedra/vendor-dir))
 
