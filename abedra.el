a8911832eaeb66680317ba26eb82e20ed8eedc28
Aaron Bedra <aaron@aaronbedra.com>
2015-06-30 20:08:11 -0500

    Regen abedra.el

--- abedra.el
+++ abedra.el
@@ -450,3 +450,10 @@
 (if window-system
     (load-theme 'solarized-light t)
   (load-theme 'wombat t))
+
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
